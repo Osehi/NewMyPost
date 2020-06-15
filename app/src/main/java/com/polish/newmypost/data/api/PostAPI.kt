7dc763cf59a63505918275be279663c5b49e495a
Osehi <osehiaseehilen@gmail.com>
2020-06-15 10:20:31 +0100

    PostAPI is created

--- app/src/main/java/com/polish/newmypost/data/api/PostAPI.kt
+++ app/src/main/java/com/polish/newmypost/data/api/PostAPI.kt
@@ -1,7 +1,32 @@
 package com.polish.newmypost.data.api
 
+import com.polish.newmypost.constants.URLConstants
+import com.polish.newmypost.constants.URLEndpoints
+import com.polish.newmypost.model.POST
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
 interface PostAPI {
 
-    fun viewPosts():List<POST>
+    @GET(URLEndpoints.POST_ENDPOINT)
+   suspend fun viewPosts():List<POST>
+
+    companion object {
+
+        operator fun invoke():PostAPI {
+
+            val client = OkHttpClient.Builder().build()
+            return Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(URLConstants.BASE_URL)
+                .client(client)
+                .build()
+                .create(PostAPI::class.java)
+
+        }
+
+    }
 
 }
